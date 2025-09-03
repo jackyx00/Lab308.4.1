71a73c22d0ab66eec15b67fc47fbb3895da873a6
Ying Jie Mei <jackymei95@gmail.com>
2025-09-03 16:50:23 -0400

    finished part 4

--- script.js
+++ script.js
@@ -34,7 +34,7 @@
         }
         console.log(data);
 
-        
+
 /* Part 2 */
 
 // Split into rows by "\n"
@@ -45,8 +45,60 @@ let table = [];
 
 // Split into cells by ","
 for (let i = 0; i < rows.length; i++) {
-  let cells = rows[i].split(",");
-  table.push(cells);
+    let cells = rows[i].split(",");
+    table.push(cells);
 }
 
 console.log(table);
+
+
+/* Part 3 */
+
+// Take the headers out to array keys by looking at only row table[0]
+let keys = [];
+for (let i = 0; i < table[0].length; i++) {
+    keys.push(table[0][i].toLowerCase());
+}
+
+// a new objects array to store all objects
+let objects = [];
+// start i = 1 because no header row
+for (let i = 1; i < table.length; i++) {
+    let table_row = table[i]; // each row in table
+    let obj = {}; // empty object
+    for (let j = 0; j < keys.length; j++) {
+        // assign values of their respective keys array to obj
+        obj[keys[j]] = table_row[j];
+    }
+    objects.push(obj);
+}
+
+console.log(objects);
+
+
+/* Part 4 */
+
+objects.pop();
+console.log(objects);
+
+let Barry = { id: "48", name: "Barry", occupation: "Runner", age: "25" };
+let Bilbo = { id: "7", name: "Bilbo", occupation: "None", age: "111" };
+
+objects.splice(1, 0, Barry);
+console.log(objects);
+
+objects.push(Bilbo);
+console.log(objects);
+
+let average_age = 0;
+for (let i = 0; i < objects.length; i++) {
+    average_age += parseInt(objects[i].age);
+    if (i === objects.length - 1) {
+        average_age / objects.length;
+    }
+}
+console.log(`The average age of the group is ${average_age}`);
+
+
+/* Part 5 */
+
